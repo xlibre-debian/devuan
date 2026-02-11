3f090915d8c27a4afc77926d86979d5aef5aeb0f
NexusSfan <nexussfan@duck.com>
2026-02-11 15:40:23 -0800

    Revert "Xserver 25.1.2"
    This reverts commit ed2c66c7f3a5497c078aa8ed1982d01fb239ebb4.
    
    Doesn't work on SDDM AND LightDM. Sorry.

--- include.sh
+++
@@ -1,7 +0,0 @@
-reprepro -C stable includedeb main ~/XLibreDebianRemote/devuan-packages-amd64/*.deb
-reprepro -C stable includedeb main ~/XLibreDebianRemote/devuan-packages-arm64/*.deb
-reprepro -C testing includedeb main ~/XLibreDebianRemote/devuan_testing-packages-amd64/*.deb
-reprepro -C testing includedeb main ~/XLibreDebianRemote/devuan_testing-packages-arm64/*.deb
-reprepro export
-reprepro deleteunreferenced
-reprepro export
